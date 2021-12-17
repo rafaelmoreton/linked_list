79b29ccff524d6a4615351d74dc2ec668b46a4ce
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2021-12-17 13:14:04 -0300

    #to_s bug correction and more LinkedList methods The #to_s now correctly prints the whole list Implemented the #insert_at and #remove_at methods. They work as intended if the index value provided as argument is within the list's size. Both could benefit from a recue clause.

--- linked_list.rb
+++ linked_list.rb
@@ -1,40 +1,45 @@
+# frozen_string_literal: true
+
+# Used to store the head and tail of this single linked list, as well as the
+# methods to operate on them and the nodes - instances of the Node class.
 class LinkedList
   attr_reader :head, :tail
+
   def initialize
     @head = nil
     @tail = nil
   end
 
   def append(data)
-    unless @tail.nil?
-      old_tail = @tail 
+    if @tail.nil?
+      @tail = Node.new(data)
+    else
+      old_tail = @tail
       @tail = Node.new(data)
       old_tail.next_node = @tail
-    else
-      @tail = Node.new(data)
     end
     @head = @tail if @head.nil?
     p to_s
   end
 
   def prepend(data)
-    unless @head.nil?
+    if @head.nil?
+      @head = Node.new(data)
+    else
       old_head = @head
       @head = Node.new(data)
       @head.next_node = old_head
-    else
-      @head = Node.new(data)
     end
     @tail = @head if @tail.nil?
     p to_s
   end
 
   def size
-    return 0 if @head == nil
+    return 0 if @head.nil?
 
     count = 1
     current_node = @head
-    until current_node.next_node == nil
+    until current_node.next_node.nil?
       count += 1
       current_node = current_node.next_node
     end
@@ -57,8 +62,9 @@ class LinkedList
 
   def contains?(value)
     current_node = @head
-    until current_node.next_node == nil
+    until current_node.next_node.nil?
       return true if current_node.value == value
+
       current_node = current_node.next_node
     end
     false
@@ -67,25 +73,54 @@ class LinkedList
   def find(value)
     current_node = @head
     index = 0
-    until current_node.next_node == nil
+    until current_node.next_node.nil?
       return index if current_node.value == value
+
       current_node = current_node.next_node
       index += 1
     end
-    nil
+    'nil'
   end
 
   def to_s
     current_node = @head
-    string = ""
-    loop do
+    string = ''
+    until current_node.nil? && current_node != @head
       string += "( #{current_node.value} ) -> "
       current_node = current_node.next_node
     end
-    string += "nil"
+    "#{string} nil"
+  end
+
+  def insert_at(value, position)
+    return prepend(value) if position.zero?
+
+    current_node = @head
+    index = 0
+    until index == position - 1
+      current_node = current_node.next_node
+      index += 1
+    end
+    previous_node = current_node
+    current_node = Node.new(value)
+    current_node.next_node = previous_node.next_node
+    previous_node.next_node = current_node
+  end
+
+  def remove_at(index)
+    current_node = @head
+    until index.zero?
+      previous_node = current_node
+      current_node = current_node.next_node
+      index -= 1
+    end
+    previous_node.next_node = current_node.next_node
+    current_node
   end
 end
 
+# Each of them is used to store a value (data) on the list and a pointer to the
+# next node. The tail node should point to 'nil', terminating the list.
 class Node
   attr_accessor :value, :next_node
 
@@ -95,20 +130,29 @@ class Node
   end
 end
 
-aparatos = LinkedList.new
-aparatos.append(5)
-aparatos.append(8)
-aparatos.prepend(10)
-aparatos.prepend(12)
-aparatos.append(1)
-aparatos.append(3)
+test_list = LinkedList.new
+test_list.append(5)
+test_list.append(8)
+test_list.prepend(10)
+test_list.prepend(12)
+test_list.append(1)
+test_list.append(3)
 
-
-# p "Size #{aparatos.size}"
-# p "Head #{aparatos.head.value}"
-# p "Tail #{aparatos.tail.value}"
-# p "At(2) should be 5. It is: #{aparatos.at(2).value}"
-# p "Pop tail, 3#{aparatos.pop}, new tail should be 1: #{aparatos.tail.value}; Size 5: #{aparatos.size}"
-
-# p "Contains(8) should return true: #{aparatos.contains?(8)}, Contains(3), which return false: #{aparatos.contains?(3)}"
-# p "Find(8) should return index 3: #{aparatos.find(8)}"
+p "Size #{test_list.size}"
+p "Head #{test_list.head.value}"
+p "Tail #{test_list.tail.value}"
+p "At(2) #{test_list.at(2).value}"
+p "Pop tail, 3#{test_list.pop}"
+p test_list.to_s
+p "Tail #{test_list.tail.value}"
+p "Size #{test_list.size}"
+p "Contains(8) #{test_list.contains?(8)}"
+p "Contains(3) #{test_list.contains?(3)}"
+p "Find(8) #{test_list.find(8)}"
+p "Find(15) #{test_list.find(15)}"
+p "Insert at(3) #{test_list.insert_at(111, 3).value}"
+p "Insert at(0) #{test_list.insert_at('head', 0)}"
+p test_list.to_s
+p "Insert at(5) #{test_list.insert_at(19, 5).value}"
+p "Remove at(2) #{test_list.remove_at(2).value}"
+p test_list.to_s
